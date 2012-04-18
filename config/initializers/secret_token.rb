076d137ba9bba4d7b649b5518c8a21c9e2b60db2
Patrick Robertson <patricksrobertson@gmail.com>
2012-04-17 21:37:29 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CopycopterClientApp::Application.config.secret_token = '0343cb9b56f02826dfb27956f74074b522d11bf8fab14d71f8fc337e14d563819a9ddf579471a8ab1b3ddc8c4dfcc6653b43ad919e520e645ae020f28018aaf6'
